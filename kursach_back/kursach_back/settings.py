388d532fa3dfe227ecd5da6e64fb8ebee884ee82
Egor <egoryurikov@mail.ru>
2023-02-27 22:03:48 +0300

    connect postgre

--- kursach_back/kursach_back/settings.py
+++ kursach_back/kursach_back/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'kursach_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kursach_db',
+        'USER': 'kursach_dbuser',
+        'PASSWORD': 'asdf',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
