b6dde315bc345200706bace60ab894c3b5c7cfaa
Egor <egoryurikov@mail.ru>
2023-03-05 20:30:40 +0300

    simplier company serializer

--- kursach_back/kursach_app/serializers.py
+++ kursach_back/kursach_app/serializers.py
@@ -8,26 +8,22 @@ class UserSerializer(serializers.Serializer):
         fields = '__all__'
 
 
-class CompanySerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=30)
-    ticker = serializers.CharField(max_length=5)
-    picture = serializers.CharField(max_length=100)
-    number_of_shares = serializers.IntegerField()
-    country = serializers.CharField(max_length=50)
-    currency = serializers.CharField(max_length=30, default='dollar')
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
 
-    def create(self, validated_data):
-        return Company.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Company.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.ticker = validated_data.get('ticker', instance.ticker)
-        instance.picture = validated_data.get('picture', instance.picture)
-        instance.number_of_shares = validated_data.get('number_of_shares', instance.number_of_shares)
-        instance.country = validated_data.get('country', instance.country)
-        instance.currency = validated_data.get('currency', instance.currency)
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.ticker = validated_data.get('ticker', instance.ticker)
+    #     instance.picture = validated_data.get('picture', instance.picture)
+    #     instance.number_of_shares = validated_data.get('number_of_shares', instance.number_of_shares)
+    #     instance.country = validated_data.get('country', instance.country)
+    #     instance.currency = validated_data.get('currency', instance.currency)
         
-        instance.save()
+    #     instance.save()
         
-        return instance
+    #     return instance
