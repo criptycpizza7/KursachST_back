f93057e4a2a4660be9f2793f5ca6744a037fbbfd
Egor <egoryurikov@mail.ru>
2023-03-05 14:33:04 +0300

    lower case fieldnames

--- kursach_back/kursach_app/models.py
+++ kursach_back/kursach_app/models.py
@@ -4,27 +4,27 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Company(models.Model):
-    Name = models.CharField(max_length=30, unique=True)
-    Ticker = models.CharField(max_length=5, unique=True)
-    Picture = models.CharField(max_length=100, null=True)
-    Number_of_shares = models.IntegerField()
-    Country = models.CharField(max_length=50)
-    Currency = models.CharField(max_length=30, default='dollar')
+    name = models.CharField(max_length=30, unique=True)
+    ticker = models.CharField(max_length=5, unique=True)
+    picture = models.CharField(max_length=100, null=True)
+    number_of_shares = models.IntegerField()
+    country = models.CharField(max_length=50)
+    currency = models.CharField(max_length=30, default='dollar')
 
     class Meta:
         verbose_name_plural = 'Companies'
 
     def __str__(self):
-        return f"{self.Name} - {self.Ticker}"
+        return f"{self.name} - {self.ticker}"
 
 class Stocks(models.Model):
-    Time = models.DateTimeField(primary_key=True, auto_now_add=True)
-    Price = models.FloatField()
-    Company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
-    Change_percent = models.FloatField()
+    time = models.DateTimeField(primary_key=True, auto_now_add=True)
+    price = models.FloatField()
+    company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
+    change_percent = models.FloatField()
 
 class User(AbstractUser):
-    Status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(null=True)
 
@@ -32,20 +32,20 @@ class User(AbstractUser):
         return f"{self.username}"
 
 class Portfolio(models.Model):
-    User_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    Company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
-    Number_of_shares = models.IntegerField()
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
+    number_of_shares = models.IntegerField()
 
     class Meta:
         verbose_name_plural = 'Portfolio'
 
     
 class Operations(models.Model):
-    User_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    Company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
-    Date_time = models.DateTimeField(auto_now_add=True)
-    Number_of_shares = models.IntegerField(default=0)
-    Price = models.FloatField()
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
+    date_time = models.DateTimeField(auto_now_add=True)
+    number_of_shares = models.IntegerField(default=0)
+    price = models.FloatField()
 
     class Meta:
         verbose_name_plural = 'Operations'
