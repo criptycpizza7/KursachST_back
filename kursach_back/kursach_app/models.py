7b2f42ebc87a034a37195d8355698f5500d5e1df
Egor <egoryurikov@mail.ru>
2023-03-10 22:41:20 +0300

    deleted is active from user model

--- kursach_back/kursach_app/models.py
+++ kursach_back/kursach_app/models.py
@@ -30,7 +30,7 @@ class Stocks(models.Model):
 class User(AbstractUser):
     status = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
-    is_active = models.BooleanField(null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.username}"
