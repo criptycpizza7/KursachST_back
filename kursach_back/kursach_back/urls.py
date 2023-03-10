9477c0245b3afe450e5dcee80826c3faec399d01
Egor <egoryurikov@mail.ru>
2023-03-10 19:48:50 +0300

    added jwt

--- kursach_back/kursach_back/urls.py
+++ kursach_back/kursach_back/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from kursach_app import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
@@ -26,5 +31,7 @@ urlpatterns = [
     path("portfolio/", views.GetPortfolioOfUser.as_view()),
     path('stocks/', views.GetStocksByCompany.as_view()),
     path('operations/', views.GetOperations.as_view()),
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
