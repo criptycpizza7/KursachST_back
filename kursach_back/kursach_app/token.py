8e743910c726c749af837ed7e8801a21d4fa656b
Egor <egoryurikov@mail.ru>
2023-05-02 19:37:27 +0300

    fixed verify

--- kursach_back/kursach_app/token.py
+++ kursach_back/kursach_app/token.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenViewBase
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework.response import Response
 import jwt
+from jwt.exceptions import InvalidSignatureError, ExpiredSignatureError
 
 from .models import User
 
@@ -25,18 +26,26 @@ class MyTokenVerifyView(TokenViewBase):
     serializer_class = TokenVerifySerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-
-        user_id = jwt.decode(request.data['token'], key='django-insecure-v96(ul6q_=kr!kmcj-rpu5@0n0&pa^0q&r$mtb1t9-4zwrhstn', algorithms='HS256')['user_id']
-
-        user = User.objects.get(pk=user_id)
-
         try:
-            serializer.is_valid(raise_exception=True)
-        except TokenError as e:
-            raise InvalidToken(e.args[0])
 
-        return Response(model_to_dict(user, fields=['id', 'is_staff', 'username']))
+            serializer = self.get_serializer(data=request.data)
+
+            user_id = jwt.decode(request.data['token'], key='django-insecure-v96(ul6q_=kr!kmcj-rpu5@0n0&pa^0q&r$mtb1t9-4zwrhstn', algorithms='HS256')['user_id']
+
+            user = User.objects.get(pk=user_id)
+
+            try:
+                serializer.is_valid(raise_exception=True)
+            except TokenError as e:
+                raise InvalidToken(e.args[0])
+
+            return Response(model_to_dict(user, fields=['id', 'is_staff', 'username']))
+        
+        except InvalidSignatureError:
+            return Response({'error': 'Токен не валиден'})
+        
+        except ExpiredSignatureError:
+            return Response({'error': 'Срок жизни токена истёк'})
     
 
 class MyTokenObtainPair(TokenViewBase):
